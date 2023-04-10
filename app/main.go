c24c14e5e681d78bad8e219cca50d75aefb22cf4
Wojciech Matuszewski <wm.matuszewski@gmail.com>
2023-04-10 07:00:45 +0200

    update

--- app/main.go
+++ app/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"io"
 	"os"
 	"os/exec"
+	"path"
+	"path/filepath"
+	"syscall"
 )
 
 func main() {
@@ -23,8 +27,28 @@ func main() {
 func run(command *exec.Cmd, stdout io.Writer, stderr io.Writer) (int, error) {
 	command.Stdout = stdout
 	command.Stderr = stderr
+	command.Stdin = os.Stdin
 
-	err := command.Run()
+	tmpDir := path.Join(os.TempDir(), "mydocker")
+	err := os.Mkdir(tmpDir, 0744)
+	if err != nil {
+		return 0, fmt.Errorf("failed to create temporary directory: %w", err)
+	}
+
+	err = exec.Command("mkdir", "-p", filepath.Join(tmpDir, filepath.Dir(command.Args[0]))).Run()
+	if err != nil {
+		return 0, fmt.Errorf("failed to create directory %s: %w", filepath.Join(tmpDir, filepath.Dir(command.Args[0])), err)
+	}
+
+	err = exec.Command("cp", command.Args[0], path.Join(tmpDir, command.Args[0])).Run()
+	if err != nil {
+		return 0, fmt.Errorf("failed to copy binary %s to chroot directory %s: %w", command.Args[0], tmpDir, err)
+	}
+
+	command.SysProcAttr = &syscall.SysProcAttr{
+		Chroot: tmpDir,
+	}
+	err = command.Run()
 	var exitErr *exec.ExitError
 	if errors.As(err, &exitErr) {
 		return exitErr.ExitCode(), nil
